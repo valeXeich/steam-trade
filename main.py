321f2a49d6f456023b9bd80654109bd9daa5a12b
valex <valerapanow03@gmail.com>
2022-09-01 17:42:45 +0300

    start

---
+++ main.py
@@ -0,0 +1,10 @@
+from PyQt5 import QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi()
+
+    def setupUi(self):
+        pass

45ff037e2820e3bb69d92f14dab4d3ec5df48fd5
hirogarugaru <someguytracer@gmail.com>
2022-10-11 20:36:57 +0300

    config fix

--- config.py
+++ config.py
@@ -1,19 +1,21 @@
 from pathlib import Path
 
+path = Path(__file__).parent
+
 if 'dist' in __file__:
-    PATH_TO_DB = f"{Path(__file__).parent}/sta.db"
-    PATH_TO_STYLES = f"{Path(__file__).parent}/ui/styles/"
-    PATH_TO_ASSETS = f"{Path(__file__).parent}/ui/assets/"
+    PATH_TO_DB = f"{path}/sta.db"
+    PATH_TO_STYLES = f"{path}/ui/styles/"
+    PATH_TO_ASSETS = f"{path}/ui/assets/"
     
     PATH_TO_GET_JSON_BUTTON_STYLES = f"{PATH_TO_STYLES}GetJSONButton.qss"
     PATH_TO_GUARD_BUTTON_STYLES = f"{PATH_TO_STYLES}GuardButton.qss"
     PATH_TO_SETTINGS_BUTTON_STYLES = f"{PATH_TO_STYLES}SettingsButton.qss"
 else:
-    PATH_TO_DB = f"{Path(__file__).parent}/core/db/sta.db"
-    PATH_TO_STYLES = f"{Path(__file__).parent}/ui/styles/"
-    PATH_TO_ASSETS = f"{Path(__file__).parent}/ui/assets/"
+    PATH_TO_DB = f"{path}/core/db/sta.db"
+    PATH_TO_STYLES = f"{path}/ui/styles/"
+    PATH_TO_ASSETS = f"{path}/ui/assets/"
     
-    PATH_TO_COMPONENTS_BUTTONS = f"{Path(__file__).parent}/ui/components/buttons/"
+    PATH_TO_COMPONENTS_BUTTONS = f"{path}/ui/components/buttons/"
     PATH_TO_GET_JSON_BUTTON_STYLES = f"{PATH_TO_COMPONENTS_BUTTONS}GetJSONButton/GetJSONButton.qss"
     PATH_TO_GUARD_BUTTON_STYLES = f"{PATH_TO_COMPONENTS_BUTTONS}GuardButton/GuardButton.qss"
     PATH_TO_SETTINGS_BUTTON_STYLES = f"{PATH_TO_COMPONENTS_BUTTONS}SettingsButton/SettingsButton.qss"
